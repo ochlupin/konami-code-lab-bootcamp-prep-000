06f9d4ac8cc2e5e3894c2addec1550fc4eb5f289
Vu Phong Nguyen <vuphongnguyen87@gmail.com>
2018-05-02 16:35:58 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -2,7 +2,7 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 let index = 0;
 function init() {
-document.body.addEventListener("keydown",)
+document.body.addEventListener("keydown",onKeyDownHandler)
 function onKeyDownHandler(e){
   const input=parseInt(e.detail||e.which);
   if (input===code[index]){
